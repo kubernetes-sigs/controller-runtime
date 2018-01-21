136d34e5d5c37bb79df3369aa0e7a8cdbcfbdad7
Hannes Hörl <hhorl@pivotal.io>
2018-01-21 01:41:31 +0000

    Update docs for KubeCtl

--- integration/kubectl.go
+++ integration/kubectl.go
@@ -7,15 +7,23 @@ import (
 	"github.com/kubernetes-sig-testing/frameworks/integration/internal"
 )
 
+// KubeCtl is a wrapper around the kubectl binary.
 type KubeCtl struct {
+	// Path where the kubectl binary can be found. If left empty, the
+	// BinPathFinder will be used to determine the path to the binary.
 	Path string
 
+	// Opts can be used to configure certain option which should be used each
+	// time the wrapped binary is called.
+	Opts []string
+
+	// Stdout & Stderr capture and store both Stdout & Stderr of the binary.
 	Stdout []byte
 	Stderr []byte
-
-	Opts []string
 }
 
+// Run executes the wrapped binary with some preconfigured options and the
+// arguments given to this method.
 func (k *KubeCtl) Run(args ...string) error {
 	if k.Path == "" {
 		k.Path = internal.BinPathFinder("kubectl")

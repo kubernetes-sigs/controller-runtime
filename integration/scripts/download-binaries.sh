4875cd9996927c108296db3b4eb73085c34c6b0c
Maru Newby <marun@redhat.com>
2018-01-28 16:54:10 -0800

    Switch download script to use curl
    curl is installed on more platforms (darwin, centos, fedora) compared
    to wget.

--- integration/scripts/download-binaries.sh
+++ integration/scripts/download-binaries.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # Use DEBUG=1 ./scripts/download-binaries.sh to get debug output
-quiet="--quiet"
+quiet="-s"
 [[ -z "${DEBUG:-""}" ]] || {
   set -x
   quiet=""
@@ -18,12 +18,12 @@ os_lowercase="$(echo "$os" | tr '[:upper:]' '[:lower:]' )"
 arch="$(uname -m)"
 
 echo "About to download a couple of binaries. This might take a while..."
-wget $quiet "${BASE_URL}/etcd-${os}-${arch}" -O "${test_framework_dir}/assets/bin/etcd"
-wget $quiet "${BASE_URL}/kube-apiserver-${os}-${arch}" -O "${test_framework_dir}/assets/bin/kube-apiserver"
+curl $quiet "${BASE_URL}/etcd-${os}-${arch}" --output "${test_framework_dir}/assets/bin/etcd"
+curl $quiet "${BASE_URL}/kube-apiserver-${os}-${arch}" --output "${test_framework_dir}/assets/bin/kube-apiserver"
 
-kubectl_version="$(wget --quiet -O - https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
+kubectl_version="$(curl https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
 kubectl_url="https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/${os_lowercase}/amd64/kubectl"
-wget $quiet "$kubectl_url" -O "${test_framework_dir}/assets/bin/kubectl"
+curl ${quiet} "$kubectl_url" --output "${test_framework_dir}/assets/bin/kubectl"
 
 chmod +x "${test_framework_dir}/assets/bin/etcd"
 chmod +x "${test_framework_dir}/assets/bin/kube-apiserver"

dd2d527c91940700ebdb3575553e87cdf76debaf
Hannes Hörl <hhorl@pivotal.io>
2018-01-31 12:37:01 +0000

    Make APIServer's StartMsg aware of secure connections

--- integration/internal/apiserver_test.go
+++ integration/internal/apiserver_test.go
@@ -42,3 +42,26 @@ var _ = Describe("Apiserver", func() {
 		})
 	})
 })
+
+var _ = Describe("GetAPIServerStartMessage()", func() {
+	Context("when using a non tls URL", func() {
+		It("generates valid start message", func() {
+			url := url.URL{
+				Scheme: "http",
+				Host:   "some.insecure.apiserver:1234",
+			}
+			message := GetAPIServerStartMessage(url)
+			Expect(message).To(Equal("Serving insecurely on some.insecure.apiserver:1234"))
+		})
+	})
+	Context("when using a tls URL", func() {
+		It("generates valid start message", func() {
+			url := url.URL{
+				Scheme: "https",
+				Host:   "some.secure.apiserver:8443",
+			}
+			message := GetAPIServerStartMessage(url)
+			Expect(message).To(Equal("Serving securely on some.secure.apiserver:8443"))
+		})
+	})
+})

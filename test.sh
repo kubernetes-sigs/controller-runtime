40302c5080a483653066d59042ee969d500b06cc
Phillip Wittrock <pwittroc@google.com>
2018-06-07 12:13:15 -0700

    Update test.sh to fetch tools

--- test.sh
+++ test.sh
@@ -25,4 +25,62 @@ if [ -n "$TRACE" ]; then
   set -x
 fi
 
+# Turn colors in this script off by setting the NO_COLOR variable in your
+# environment to any value:
+#
+# $ NO_COLOR=1 test.sh
+NO_COLOR=${NO_COLOR:-""}
+if [ -z "$NO_COLOR" ]; then
+  header=$'\e[1;33m'
+  reset=$'\e[0m'
+else
+  header=''
+  reset=''
+fi
+
+tmp_root=/tmp
+kb_root_dir=$tmp_root/kubebuilder
+
+# fetch the testing binaries - e.g. apiserver and etcd
+fetch_kb_tools
+
+# setup testing env
+setup_envs
+
 go test github.com/kubernetes-sigs/controller-runtime/pkg/...
+go build github.com/kubernetes-sigs/controller-runtime/example
+
+# Skip fetching and untaring the tools by setting the SKIP_FETCH_TOOLS variable
+# in your environment to any value:
+#
+# $ SKIP_FETCH_TOOLS=1 ./test.sh
+#
+# If you skip fetching tools, this script will use the tools already on your
+# machine, but rebuild the kubebuilder and kubebuilder-bin binaries.
+SKIP_FETCH_TOOLS=${SKIP_FETCH_TOOLS:-""}
+
+# fetch k8s API gen tools and make it available under kb_root_dir/bin.
+function fetch_kb_tools {
+  if [ -n "$SKIP_FETCH_TOOLS" ]; then
+    return 0
+  fi
+
+  header_text "fetching tools"
+  kb_tools_archive_name="kubebuilder-tools-$k8s_version-$goos-$goarch.tar.gz"
+  kb_tools_download_url="https://storage.googleapis.com/kubebuilder-tools/$kb_tools_archive_name"
+
+  kb_tools_archive_path="$tmp_root/$kb_tools_archive_name"
+  if [ ! -f $kb_tools_archive_path ]; then
+    curl -sL ${kb_tools_download_url} -o "$kb_tools_archive_path"
+  fi
+  tar -zvxf "$kb_tools_archive_path" -C "$tmp_root/"
+}
+
+function setup_envs {
+  header_text "setting up env vars"
+
+  # Setup env vars
+  export TEST_ASSET_KUBECTL=$kb_root_dir/bin/kubectl
+  export TEST_ASSET_KUBE_APISERVER=$kb_root_dir/bin/kube-apiserver
+  export TEST_ASSET_ETCD=$kb_root_dir/bin/etcd
+}

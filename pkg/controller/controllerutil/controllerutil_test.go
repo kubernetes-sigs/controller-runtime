e48b1797be6a7c026566873e541c2624ddd292ed
Phillip Wittrock <pwittroc@google.com>
2018-06-19 18:06:50 -0700

    Add helper function for adding OwnerReferences to metav1.Objects

---
+++ pkg/controller/controllerutil/controllerutil_test.go
@@ -0,0 +1,52 @@
+package controllerutil
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+)
+
+var _ = Describe("Controllerutil", func() {
+	Describe("SetControllerReference", func() {
+		It("should set the OwnerReference if it can find the group version kind", func() {
+			rs := &appsv1.ReplicaSet{}
+			dep := &extensionsv1beta1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{Name: "foo", UID: "foo-uid"},
+			}
+
+			Expect(SetControllerReference(dep, rs, scheme.Scheme)).NotTo(HaveOccurred())
+			t := true
+			Expect(rs.OwnerReferences).To(ConsistOf(metav1.OwnerReference{
+				Name:               "foo",
+				Kind:               "Deployment",
+				APIVersion:         "extensions/v1beta1",
+				UID:                "foo-uid",
+				Controller:         &t,
+				BlockOwnerDeletion: &t,
+			}))
+		})
+
+		It("should return an error if it can't find the group version kind of the owner", func() {
+			rs := &appsv1.ReplicaSet{}
+			dep := &extensionsv1beta1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{Name: "foo"},
+			}
+			Expect(SetControllerReference(dep, rs, runtime.NewScheme())).To(HaveOccurred())
+		})
+
+		It("should return an error if the owner isn't a runtime.Object", func() {
+			rs := &appsv1.ReplicaSet{}
+			Expect(SetControllerReference(&errMetaObj{}, rs, scheme.Scheme)).To(HaveOccurred())
+		})
+	})
+})
+
+var _ metav1.Object = &errMetaObj{}
+
+type errMetaObj struct {
+	metav1.ObjectMeta
+}

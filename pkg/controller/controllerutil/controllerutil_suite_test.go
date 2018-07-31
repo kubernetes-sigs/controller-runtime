d47ce17dbdc6dbf83e75eda2c2a038f0db0f83c7
Calin Don <calin.don@gmail.com>
2018-07-31 13:41:01 +0300

    Add CreateOrUpdate utility method

--- pkg/controller/controllerutil/controllerutil_suite_test.go
+++ pkg/controller/controllerutil/controllerutil_suite_test.go
@@ -21,9 +21,33 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
 )
 
 func TestControllerutil(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Controllerutil Suite")
 }
+
+var t *envtest.Environment
+var cfg *rest.Config
+var c client.Client
+
+var _ = BeforeSuite(func() {
+	var err error
+
+	t = &envtest.Environment{}
+
+	cfg, err = t.Start()
+	Expect(err).NotTo(HaveOccurred())
+
+	c, err = client.New(cfg, client.Options{})
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	t.Stop()
+})

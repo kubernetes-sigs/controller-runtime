36577983d0a36a4aa30211cd9d81551bd91d6549
Mengqi Yu <mengqiy@google.com>
2018-09-11 10:01:50 -0700

    change how to set webhook type and how we default path in builder

--- example/mutatingwebhook.go
+++ example/mutatingwebhook.go
@@ -59,8 +59,11 @@ func mutatePodsFn(ctx context.Context, pod *corev1.Pod) error {
 	if !ok {
 		return fmt.Errorf("the value associated with %v is expected to be a string", "foo")
 	}
-	anno := pod.GetAnnotations()
+	anno := pod.Annotations
+	if anno == nil {
+		anno = map[string]string{}
+	}
 	anno["example-mutating-admission-webhook"] = v
-	pod.SetAnnotations(anno)
+	pod.Annotations = anno
 	return nil
 }

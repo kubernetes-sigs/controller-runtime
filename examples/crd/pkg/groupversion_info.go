089333274ea42570b399f95e43372e44f7fc59dc
Mengqi Yu <mengqiy@google.com>
2019-05-14 15:13:16 -0700

    :running: use right markers

--- examples/crd/pkg/groupversion_info.go
+++ examples/crd/pkg/groupversion_info.go
@@ -14,10 +14,25 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// +kubebuilder:object:generate=true
+// +groupName=chaosapps.metamagical.io
 package pkg
 
 import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
-var log = logf.Log.WithName("chaospod-resource")
+var (
+	log = logf.Log.WithName("chaospod-resource")
+
+	// SchemeGroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{Group: "chaosapps.metamagical.io", Version: "v1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+
+	// AddToScheme is required by pkg/client/...
+	AddToScheme = SchemeBuilder.AddToScheme
+)

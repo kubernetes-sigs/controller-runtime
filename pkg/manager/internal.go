e63210a56548cc04c5438a1471c54f59c482295d
adohe <coolhzb@163.com>
2019-06-13 11:57:06 +0800

    :bug: fix runnable run twice issue

--- pkg/manager/internal.go
+++ pkg/manager/internal.go
@@ -91,9 +91,10 @@ type controllerManager struct {
 	// metricsListener is used to serve prometheus metrics
 	metricsListener net.Listener
 
-	mu      sync.Mutex
-	started bool
-	errChan chan error
+	mu            sync.Mutex
+	started       bool
+	startedLeader bool
+	errChan       chan error
 
 	// internalStop is the stop channel *actually* used by everything involved
 	// with the manager as a stop channel, so that we can pass a stop channel
@@ -135,14 +136,18 @@ func (cm *controllerManager) Add(r Runnable) error {
 		return err
 	}
 
+	var shouldStart bool
+
 	// Add the runnable to the leader election or the non-leaderelection list
 	if leRunnable, ok := r.(LeaderElectionRunnable); ok && !leRunnable.NeedLeaderElection() {
+		shouldStart = cm.started
 		cm.nonLeaderElectionRunnables = append(cm.nonLeaderElectionRunnables, r)
 	} else {
+		shouldStart = cm.startedLeader
 		cm.leaderElectionRunnables = append(cm.leaderElectionRunnables, r)
 	}
 
-	if cm.started {
+	if shouldStart {
 		// If already started, start the controller
 		go func() {
 			cm.errChan <- r.Start(cm.internalStop)
@@ -316,6 +321,8 @@ func (cm *controllerManager) startLeaderElectionRunnables() {
 			cm.errChan <- ctrl.Start(cm.internalStop)
 		}()
 	}
+
+	cm.startedLeader = true
 }
 
 func (cm *controllerManager) waitForCache() {

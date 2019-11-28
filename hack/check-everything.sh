df410204905b592959f41180b3bf49f48cd99a3c
Camila Macedo <camilamacedo86@gmail.com>
2019-11-28 17:35:35 +0000

    Change the golint install to do it as recommended

--- hack/check-everything.sh
+++ hack/check-everything.sh
@@ -70,7 +70,7 @@ function fetch_go_tools {
   header_text "Checking for gometalinter.v2"
   if ! is_installed golangci-lint; then
     header_text "Installing golangci-lint"
-    GO111MODULE=on go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.17.1
+    curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b $(go env GOPATH)/bin v1.21.0
   fi
 }
 

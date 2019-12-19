8b6450d9f16d64e3357204d39e4bbda2d9609b9b
Ace Eldeib <alexeldeib@gmail.com>
2019-12-18 16:02:25 -0800

    fail via bash?

--- hack/release/verify-emoji.sh
+++ hack/release/verify-emoji.sh
@@ -25,4 +25,10 @@ base_link=$(jq -r '.pull_request.url' < ${GITHUB_EVENT_PATH})
 head_commit=$(curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.antiope-preview+json' -q ${base_link} | jq -r '.head.sha')
 echo "head commit is ${head_commit}"
 
-curl --fail https://api.github.com/repos/${GITHUB_REPOSITORY}/check-runs -XPOST -H "Authorization: Bearer ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.antiope-preview+json' -H 'Content-Type: application/json' -q --data-raw '{"name": "Verify Emoji", "head_sha": "'${head_commit}'", "conclusion": "'${conclusion}'", "status": "completed", "completed_at": "'$(date -Iseconds)'", "output": {"title": "Verify Emoji", "summary": "'"${summary}"'"}}'
+echo ${summary}
+
+if [[ ${conclusion} == "failure" ]]; then
+    exit 1
+else
+    exit 0
+fi

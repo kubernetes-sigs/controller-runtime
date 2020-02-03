cbaa31aced173092707ca9920f3c9282e72c461f
Amin Chawki <amin.chawki@sap.com>
2020-02-03 12:55:39 +0100

    Create wrapper to explictly set cache
    - sourceWithFixedCache does not implement InjectCache
    - it does not rely on the current implicit implementation that a cache
    is not overwriting in case it was injected before
    - see #650
    
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- pkg/source/source.go
+++ pkg/source/source.go
@@ -55,6 +55,21 @@ type Source interface {
 	Start(handler.EventHandler, workqueue.RateLimitingInterface, ...predicate.Predicate) error
 }
 
+// CreateSourceWithFixedCache creates a Source without InjectCache, so that it is assured that the given cache is used
+// and not overwritten
+func CreateSourceWithFixedCache(object runtime.Object, cache cache.Cache) Source {
+	return &sourceWithFixedCache{kind: Kind{Type: object, cache: cache}}
+}
+
+type sourceWithFixedCache struct {
+	kind Kind
+}
+
+func (ks *sourceWithFixedCache) Start(handler handler.EventHandler, queue workqueue.RateLimitingInterface,
+	prct ...predicate.Predicate) error {
+	return ks.kind.Start(handler, queue, prct...)
+}
+
 // Kind is used to provide a source of events originating inside the cluster from Watches (e.g. Pod Create)
 type Kind struct {
 	// Type is the type of object to watch.  e.g. &v1.Pod{}

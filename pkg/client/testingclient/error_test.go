035f58cdd6a820a3fe9b7b687d90d4b8b55daa47
David Sharp <dsharp@vmware.com>
2021-01-22 15:50:55 -0800

    testingclient.Error: Add tests for Create, Delete, Update, and Patch

--- pkg/client/testingclient/error_test.go
+++ pkg/client/testingclient/error_test.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -42,27 +43,155 @@ var _ = Describe("ErrorInjector", func() {
 		subject = testingclient.NewErrorInjector(fakeClient)
 	})
 
-	It("can inject errors on calls to Get", func() {
-		subject.InjectError("get", &corev1.Pod{}, client.ObjectKey{Name: "pod1", Namespace: "ns"}, exampleError)
+	Describe("Get", func() {
+		It("can inject errors on calls to Get", func() {
+			subject.InjectError("get", &corev1.Pod{}, client.ObjectKey{Name: "pod1", Namespace: "ns"}, exampleError)
 
-		key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
-		var obj corev1.Pod
-		Expect(subject.Get(nil, key, &obj)).To(MatchError("injected error"))
+			key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+			var obj corev1.Pod
+			Expect(subject.Get(nil, key, &obj)).To(MatchError("injected error"))
+		})
+
+		It("calls the delegate client if no errors match", func() {
+			subject.InjectError("get", &corev1.Service{}, testingclient.AnyObject, exampleError)
+
+			key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+			var obj corev1.Pod
+			Expect(subject.Get(nil, key, &obj)).To(Succeed())
+
+			var objInDelegate corev1.Pod
+			Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+
+			Expect(obj).To(Equal(objInDelegate), "obj should be the retrieved object")
+		})
 	})
 
-	It("calls the delegate client if no errors match", func() {
-		subject.InjectError("get", &corev1.Service{}, testingclient.AnyObject, exampleError)
+	Describe("Create", func() {
+		pod3 := corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "pod3",
+				Namespace: "ns",
+			},
+		}
+		It("can inject errors on calls to Create", func() {
+			subject.InjectError("create", &corev1.Pod{}, client.ObjectKey{Name: "pod3", Namespace: "ns"}, exampleError)
 
-		key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
-		var obj corev1.Pod
-		Expect(subject.Get(nil, key, &obj)).To(Succeed())
+			Expect(subject.Create(nil, &pod3)).To(MatchError("injected error"))
 
-		var objInDelegate corev1.Pod
-		Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+			key := types.NamespacedName{Namespace: "ns", Name: "pod3"}
+			var objInDelegate corev1.Pod
+			err := fakeClient.Get(nil, key, &objInDelegate)
+			Expect(apierrors.IsNotFound(err)).To(BeTrue(), "should not create the object if an error was injected")
+		})
 
-		Expect(obj).To(Equal(objInDelegate), "obj should be the retrieved object")
+		It("calls the delegate client if no errors match", func() {
+			subject.InjectError("create", &corev1.Service{}, testingclient.AnyObject, exampleError)
+
+			Expect(subject.Create(nil, &pod3)).To(Succeed())
+
+			key := types.NamespacedName{Namespace: "ns", Name: "pod3"}
+			var objInDelegate corev1.Pod
+			Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+
+			objInDelegate.TypeMeta = metav1.TypeMeta{} // Clear TypeMeta for comparison. fakeClient.Get() fills this in.
+			Expect(pod3).To(Equal(objInDelegate), "obj should be the retrieved object")
+		})
 	})
 
-	// TODO: test more functions.
+	Describe("Delete", func() {
+		It("can inject errors on calls to Delete", func() {
+			subject.InjectError("delete", &corev1.Pod{}, client.ObjectKey{Name: "pod1", Namespace: "ns"}, exampleError)
+
+			key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+			var obj corev1.Pod
+			Expect(fakeClient.Get(nil, key, &obj)).To(Succeed())
+			Expect(subject.Delete(nil, &obj)).To(MatchError("injected error"))
+
+			var objInDelegate corev1.Pod
+			Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+			Expect(obj).To(Equal(objInDelegate), "pod1 should still exist")
+		})
+
+		It("calls the delegate client if no errors match", func() {
+			subject.InjectError("delete", &corev1.Service{}, testingclient.AnyObject, exampleError)
+
+			key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+			obj := corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: key.Namespace, Name: key.Name,
+				},
+			}
+			Expect(subject.Delete(nil, &obj)).To(Succeed())
+
+			var objInDelegate corev1.Pod
+			err := fakeClient.Get(nil, key, &objInDelegate)
+			Expect(apierrors.IsNotFound(err)).To(BeTrue(), "should delete the object if no error was injected")
+
+		})
+	})
+
+	Describe("Update", func() {
+		It("can inject errors on calls to Update", func() {
+			subject.InjectError("update", &corev1.Pod{}, client.ObjectKey{Name: "pod1", Namespace: "ns"}, exampleError)
+
+			key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+			var obj corev1.Pod
+			Expect(fakeClient.Get(nil, key, &obj)).To(Succeed())
+			origObj := obj.DeepCopy()
+			obj.Spec.NodeName = "lymph"
+			Expect(subject.Update(nil, &obj)).To(MatchError("injected error"))
+
+			var objInDelegate corev1.Pod
+			Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+			Expect(objInDelegate).To(Equal(*origObj), "pod1 should be unmodified")
+		})
+
+		It("calls the delegate client if no errors match", func() {
+			subject.InjectError("update", &corev1.Service{}, testingclient.AnyObject, exampleError)
+
+			key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+			var obj corev1.Pod
+			Expect(fakeClient.Get(nil, key, &obj)).To(Succeed())
+			obj.Spec.NodeName = "lymph"
+			Expect(subject.Update(nil, &obj)).To(Succeed())
+
+			var objInDelegate corev1.Pod
+			Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+			Expect(objInDelegate).To(Equal(obj), "pod1 should be modified")
+		})
+	})
+
+	Describe("Patch", func() {
+		It("can inject errors on calls to Patch", func() {
+			subject.InjectError("patch", &corev1.Pod{}, client.ObjectKey{Name: "pod1", Namespace: "ns"}, exampleError)
+
+			key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+			var obj corev1.Pod
+			Expect(fakeClient.Get(nil, key, &obj)).To(Succeed())
+			origObj := obj.DeepCopy()
+			obj.Spec.NodeName = "lymph"
+			Expect(subject.Patch(nil, &obj, client.MergeFrom(origObj))).To(MatchError("injected error"))
+
+			var objInDelegate corev1.Pod
+			Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+			Expect(objInDelegate).To(Equal(*origObj), "pod1 should be unmodified")
+		})
+
+		It("calls the delegate client if no errors match", func() {
+			subject.InjectError("patch", &corev1.Service{}, testingclient.AnyObject, exampleError)
+
+			key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+			var obj corev1.Pod
+			Expect(fakeClient.Get(nil, key, &obj)).To(Succeed())
+			origObj := obj.DeepCopy()
+			obj.Spec.NodeName = "lymph"
+			Expect(subject.Patch(nil, &obj, client.MergeFrom(origObj))).To(Succeed())
+
+			var objInDelegate corev1.Pod
+			Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+			Expect(objInDelegate).To(Equal(obj), "pod1 should be modified")
+		})
+	})
+
 	// TODO: test prefers specific matches over general matches.
 })

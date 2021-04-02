45c8b3a53cb6f17e75961109658dbb9726a38405
David Sharp <dsharp@vmware.com>
2021-04-02 15:27:36 -0700

    testingclient.Reactive: Tests for Create, Delete, Update, Patch
    Signed-off-by: David Sharp <dsharp@vmware.com>

--- pkg/client/testingclient/reactive_test.go
+++ pkg/client/testingclient/reactive_test.go
@@ -7,6 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gstruct"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -150,6 +151,248 @@ var _ = Describe("Reactive", func() {
 				Expect(fakeClient.List(nil, &listInDelegate)).To(Succeed())
 
 				Expect(list).To(gstruct.PointTo(Equal(listInDelegate)), "list should be the retrieved list")
+			})
+		})
+	})
+
+	Describe("Create", func() {
+		key := types.NamespacedName{Namespace: "ns", Name: "pod3"}
+		var (
+			obj    *corev1.Pod
+			apiErr error
+		)
+		JustBeforeEach(func() {
+			obj = &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      key.Name,
+					Namespace: key.Namespace,
+				},
+			}
+			apiErr = subject.Create(nil, obj)
+		})
+
+		When("the reactor 'handles' the action", func() {
+			var reactorCalled bool
+			BeforeEach(func() {
+				reactorCalled = false
+				subject.PrependReactor(testingclient.CreateVerb, &corev1.Pod{}, func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+					reactorCalled = true
+					return true, nil, errors.New("reactor error")
+				})
+			})
+
+			It("calls the reactor", func() {
+				Expect(reactorCalled).To(BeTrue())
+				Expect(apiErr).To(MatchError("reactor error"))
+			})
+
+			It("doesn't call the delegate", func() {
+				var objInDelegate corev1.Pod
+				err := fakeClient.Get(nil, key, &objInDelegate)
+				Expect(apierrors.IsNotFound(err)).To(BeTrue(), "should not create the object")
+			})
+		})
+
+		When("the reactor doesn't 'handle' the action", func() {
+			var reactorCalled bool
+			BeforeEach(func() {
+				reactorCalled = false
+				subject.PrependReactor(testingclient.CreateVerb, &corev1.Pod{}, func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+					reactorCalled = true
+					return false, nil, nil
+				})
+			})
+
+			It("calls the reactor", func() {
+				Expect(reactorCalled).To(BeTrue())
+			})
+
+			It("calls the delegate client", func() {
+				Expect(apiErr).NotTo(HaveOccurred())
+
+				var objInDelegate corev1.Pod
+				Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+				objInDelegate.ResourceVersion = "" // Clear this for comparison. It is set by fake.Client.Create()
+
+				Expect(obj).To(gstruct.PointTo(Equal(objInDelegate)), "obj should be the retrieved object")
+			})
+		})
+	})
+
+	Describe("Delete", func() {
+		key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+		var (
+			obj    corev1.Pod
+			apiErr error
+		)
+		JustBeforeEach(func() {
+			Expect(fakeClient.Get(nil, key, &obj)).To(Succeed())
+			apiErr = subject.Delete(nil, &obj)
+		})
+
+		When("the reactor 'handles' the action", func() {
+			var reactorCalled bool
+			BeforeEach(func() {
+				reactorCalled = false
+				subject.PrependReactor(testingclient.DeleteVerb, &corev1.Pod{}, func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+					reactorCalled = true
+					return true, nil, errors.New("reactor error")
+				})
+			})
+
+			It("calls the reactor", func() {
+				Expect(reactorCalled).To(BeTrue())
+				Expect(apiErr).To(MatchError("reactor error"))
+			})
+
+			It("doesn't call the delegate", func() {
+				var objInDelegate corev1.Pod
+				Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+
+				Expect(obj).To(Equal(objInDelegate), "obj should still exist in the delegate")
+			})
+		})
+
+		When("the reactor doesn't 'handle' the action", func() {
+			var reactorCalled bool
+			BeforeEach(func() {
+				reactorCalled = false
+				subject.PrependReactor(testingclient.DeleteVerb, &corev1.Pod{}, func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+					reactorCalled = true
+					return false, nil, nil
+				})
+			})
+
+			It("calls the reactor", func() {
+				Expect(reactorCalled).To(BeTrue())
+			})
+
+			It("calls the delegate client", func() {
+				Expect(apiErr).NotTo(HaveOccurred())
+
+				var objInDelegate corev1.Pod
+				err := fakeClient.Get(nil, key, &objInDelegate)
+				Expect(apierrors.IsNotFound(err)).To(BeTrue(), "object should have been deleted")
+			})
+		})
+	})
+
+	Describe("Update", func() {
+		key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+		var (
+			obj    corev1.Pod
+			apiErr error
+		)
+		JustBeforeEach(func() {
+			Expect(fakeClient.Get(nil, key, &obj)).To(Succeed())
+			Expect(obj.Spec.Hostname).To(BeEmpty(), "consistency check")
+			updatedObj := obj.DeepCopy()
+			updatedObj.Spec.Hostname = "new-hostname"
+			apiErr = subject.Update(nil, updatedObj)
+		})
+
+		When("the reactor 'handles' the action", func() {
+			var reactorCalled bool
+			BeforeEach(func() {
+				reactorCalled = false
+				subject.PrependReactor(testingclient.UpdateVerb, &corev1.Pod{}, func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+					reactorCalled = true
+					return true, nil, errors.New("reactor error")
+				})
+			})
+
+			It("calls the reactor", func() {
+				Expect(reactorCalled).To(BeTrue())
+				Expect(apiErr).To(MatchError("reactor error"))
+			})
+
+			It("doesn't call the delegate", func() {
+				var objInDelegate corev1.Pod
+				Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+				Expect(objInDelegate.Spec.Hostname).To(BeEmpty(), "obj should not have been updated")
+			})
+		})
+
+		When("the reactor doesn't 'handle' the action", func() {
+			var reactorCalled bool
+			BeforeEach(func() {
+				reactorCalled = false
+				subject.PrependReactor(testingclient.UpdateVerb, &corev1.Pod{}, func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+					reactorCalled = true
+					return false, nil, nil
+				})
+			})
+
+			It("calls the reactor", func() {
+				Expect(reactorCalled).To(BeTrue())
+			})
+
+			It("calls the delegate client", func() {
+				Expect(apiErr).NotTo(HaveOccurred())
+
+				var objInDelegate corev1.Pod
+				Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+				Expect(objInDelegate.Spec.Hostname).To(Equal("new-hostname"), "obj should have been updated")
+			})
+		})
+	})
+
+	Describe("Patch", func() {
+		key := types.NamespacedName{Namespace: "ns", Name: "pod1"}
+		var (
+			obj    corev1.Pod
+			apiErr error
+		)
+		JustBeforeEach(func() {
+			Expect(fakeClient.Get(nil, key, &obj)).To(Succeed())
+			Expect(obj.Spec.Hostname).To(BeEmpty(), "consistency check")
+			updatedObj := obj.DeepCopy()
+			updatedObj.Spec.Hostname = "new-hostname"
+			apiErr = subject.Patch(nil, updatedObj, client.MergeFrom(&obj))
+		})
+
+		When("the reactor 'handles' the action", func() {
+			var reactorCalled bool
+			BeforeEach(func() {
+				reactorCalled = false
+				subject.PrependReactor(testingclient.PatchVerb, &corev1.Pod{}, func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+					reactorCalled = true
+					return true, nil, errors.New("reactor error")
+				})
+			})
+
+			It("calls the reactor", func() {
+				Expect(reactorCalled).To(BeTrue())
+				Expect(apiErr).To(MatchError("reactor error"))
+			})
+
+			It("doesn't call the delegate", func() {
+				var objInDelegate corev1.Pod
+				Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+				Expect(objInDelegate.Spec.Hostname).To(BeEmpty(), "obj should not have been updated")
+			})
+		})
+
+		When("the reactor doesn't 'handle' the action", func() {
+			var reactorCalled bool
+			BeforeEach(func() {
+				reactorCalled = false
+				subject.PrependReactor(testingclient.PatchVerb, &corev1.Pod{}, func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+					reactorCalled = true
+					return false, nil, nil
+				})
+			})
+
+			It("calls the reactor", func() {
+				Expect(reactorCalled).To(BeTrue())
+			})
+
+			It("calls the delegate client", func() {
+				Expect(apiErr).NotTo(HaveOccurred())
+
+				var objInDelegate corev1.Pod
+				Expect(fakeClient.Get(nil, key, &objInDelegate)).To(Succeed())
+				Expect(objInDelegate.Spec.Hostname).To(Equal("new-hostname"), "obj should have been updated")
 			})
 		})
 	})

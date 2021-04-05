0a3b4550a123c20fec0f598be528f2a637180800
rashmigottipati <chowdary.grashmi@gmail.com>
2021-04-05 13:02:18 -0400

    Initial commit for finalizer helper library
    Signed-off-by: rashmigottipati <chowdary.grashmi@gmail.com>

---
+++ pkg/finalizer/finalizer.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package finalizer
+
+import (
+	"context"
+	"fmt"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+func NewFinalizers() (Finalizers, error) {
+	return finalizers(map[string]Finalizer{}), nil
+}
+
+func (f finalizers) Register(key string, finalizer Finalizer) error {
+	if _, ok := f[key]; ok {
+		return fmt.Errorf("finalizer for key %q already registered", key)
+	}
+	f[key] = finalizer
+	return nil
+}
+
+func (f finalizers) Finalize(ctx context.Context, obj client.Object) (bool, error) {
+	needsUpdate := false
+	for key, finalizer := range f {
+		if obj.GetDeletionTimestamp() == nil && !controllerutil.ContainsFinalizer(obj, key) {
+			controllerutil.AddFinalizer(obj, key)
+			needsUpdate = true
+		} else if obj.GetDeletionTimestamp() != nil && controllerutil.ContainsFinalizer(obj, key) {
+			_, err := finalizer.Finalize(ctx, obj)
+			if err != nil {
+				return true, fmt.Errorf("finalize failed for key %q: %v", key, err)
+			}
+			controllerutil.RemoveFinalizer(obj, key)
+			needsUpdate = true
+		}
+	}
+	return needsUpdate, nil
+}

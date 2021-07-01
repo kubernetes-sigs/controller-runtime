f7418d60bdd3a919b95868c3d68617936d6f06fe
niuyueyang <niuyueyang@bigo.sg>
2021-07-01 16:04:07 +0800

    manager recorderProvider only used by resource lock use spec makeBroadcaster to overload cluster's makeBroadcaster

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -512,6 +512,13 @@ func defaultHealthProbeListener(addr string) (net.Listener, error) {
 	return ln, nil
 }
 
+func NewProvider(config *rest.Config, scheme *runtime.Scheme, logger logr.Logger, makeBroadcaster intrec.EventBroadcasterProducer) (*intrec.Provider, error) {
+	managerBroadcaster := func() (record.EventBroadcaster, bool) {
+		return record.NewBroadcaster(), true
+	}
+	return intrec.NewProvider(config, scheme, logger, managerBroadcaster)
+}
+
 // setOptionsDefaults set default values for Options fields.
 func setOptionsDefaults(options Options) Options {
 	// Allow newResourceLock to be mocked
@@ -521,7 +528,7 @@ func setOptionsDefaults(options Options) Options {
 
 	// Allow newRecorderProvider to be mocked
 	if options.newRecorderProvider == nil {
-		options.newRecorderProvider = intrec.NewProvider
+		options.newRecorderProvider = NewProvider
 	}
 
 	// This is duplicated with pkg/cluster, we need it here

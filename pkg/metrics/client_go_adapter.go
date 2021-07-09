4cbef7adcbd482bd3e14da343893df4c1a02b4cd
Eric Fried <efried@redhat.com>
2021-07-09 09:44:05 -0500

    :warning: Drop rest client request latecy metric
    Remove the `rest_client_request_latency_seconds` metric.
    
    This metric has been deprecated since kube 1.14 and [turned off in kube
    1.17](https://github.com/kubernetes/kubernetes/pull/83836).
    
    Co-Authored-By: suhanime

--- pkg/metrics/client_go_adapter.go
+++ pkg/metrics/client_go_adapter.go
@@ -18,8 +18,6 @@ package metrics
 
 import (
 	"context"
-	"net/url"
-	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	reflectormetrics "k8s.io/client-go/tools/cache"
@@ -52,13 +50,6 @@ const (
 
 var (
 	// client metrics.
-	requestLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Subsystem: RestClientSubsystem,
-		Name:      LatencyKey,
-		Help:      "Request latency in seconds. Broken down by verb and URL.",
-		Buckets:   prometheus.ExponentialBuckets(0.001, 2, 10),
-	}, []string{"verb", "url"})
-
 	requestResult = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Subsystem: RestClientSubsystem,
 		Name:      ResultKey,
@@ -127,13 +118,11 @@ func init() {
 // registerClientMetrics sets up the client latency metrics from client-go.
 func registerClientMetrics() {
 	// register the metrics with our registry
-	Registry.MustRegister(requestLatency)
 	Registry.MustRegister(requestResult)
 
 	// register the metrics with client-go
 	clientmetrics.Register(clientmetrics.RegisterOpts{
-		RequestLatency: &latencyAdapter{metric: requestLatency},
-		RequestResult:  &resultAdapter{metric: requestResult},
+		RequestResult: &resultAdapter{metric: requestResult},
 	})
 }
 
@@ -158,14 +147,6 @@ func registerReflectorMetrics() {
 // Client metrics adapters (method #1 for client-go metrics),
 // copied (more-or-less directly) from k8s.io/kubernetes setup code
 // (which isn't anywhere in an easily-importable place).
-
-type latencyAdapter struct {
-	metric *prometheus.HistogramVec
-}
-
-func (l *latencyAdapter) Observe(_ context.Context, verb string, u url.URL, latency time.Duration) {
-	l.metric.WithLabelValues(verb, u.String()).Observe(latency.Seconds())
-}
 
 type resultAdapter struct {
 	metric *prometheus.CounterVec

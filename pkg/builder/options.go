1bc3f4a8da84b39a3c4c077bc621cc04246af8ed
Maël Valais <mael@vls.dev>
2021-12-14 10:00:17 +0100

    Document in API the consequence of mixing concrete and metadata

--- pkg/builder/options.go
+++ pkg/builder/options.go
@@ -107,6 +107,20 @@ var (
 	// metav1.PartialObjectMetadata to the client when fetching objects in your
 	// reconciler, otherwise you'll end up with a duplicate structured or
 	// unstructured cache.
+	//
+	// When watching a resource with OnlyMetadata, for example the v1.Pod, you
+	// should not Get and List using the v1.Pod type. Instead, you should use
+	// the special metav1.PartialObjectMetadata type:
+	//
+	//   pod := &v1.Pod{}                             // ❌
+	//   mgr.GetClient().Get(ctx, nsAndName, pod)     // ❌
+	//
+	//   pod := &metav1.PartialObjectMetadata{}       // ✅
+	//   mgr.GetClient().Get(ctx, nsAndName, pod)     // ✅
+	//
+	// In the first case, controller-runtime will create another cache for the
+	// concrete type on top of the metadata cache; this increases memory
+	// consumption and leads to race conditions as caches are not in sync.
 	OnlyMetadata = projectAs(projectAsMetadata)
 
 	_ ForOption     = OnlyMetadata

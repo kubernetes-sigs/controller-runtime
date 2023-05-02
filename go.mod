935faeba70039b5403616e73f109f4b6b1115b9f
Vince Prignano <vincepri@redhat.com>
2023-05-02 11:37:08 -0700

    :warning: Use and provide a single dynamic rest mapper
    Signed-off-by: Vince Prignano <vincepri@redhat.com>

--- go.mod
+++ go.mod
@@ -15,7 +15,6 @@ require (
 	go.uber.org/goleak v1.2.1
 	go.uber.org/zap v1.24.0
 	golang.org/x/sys v0.7.0
-	golang.org/x/time v0.3.0
 	gomodules.xyz/jsonpatch/v2 v2.2.0
 	k8s.io/api v0.27.1
 	k8s.io/apiextensions-apiserver v0.27.1
@@ -62,6 +61,7 @@ require (
 	golang.org/x/oauth2 v0.5.0 // indirect
 	golang.org/x/term v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.7.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
--- pkg/webhook/webhook_integration_test.go
+++ pkg/webhook/webhook_integration_test.go
@@ -97,10 +97,11 @@ var _ = Describe("Webhook", func() {
 		})
 		It("should reject create request for multi-webhook that rejects all requests", func() {
 			m, err := manager.New(cfg, manager.Options{
-				Port:    testenv.WebhookInstallOptions.LocalServingPort,
-				Host:    testenv.WebhookInstallOptions.LocalServingHost,
-				CertDir: testenv.WebhookInstallOptions.LocalServingCertDir,
-				TLSOpts: []func(*tls.Config){func(config *tls.Config) {}},
+				MetricsBindAddress: "0",
+				Port:               testenv.WebhookInstallOptions.LocalServingPort,
+				Host:               testenv.WebhookInstallOptions.LocalServingHost,
+				CertDir:            testenv.WebhookInstallOptions.LocalServingCertDir,
+				TLSOpts:            []func(*tls.Config){func(config *tls.Config) {}},
 			}) // we need manager here just to leverage manager.SetFields
 			Expect(err).NotTo(HaveOccurred())
 			server := m.GetWebhookServer()

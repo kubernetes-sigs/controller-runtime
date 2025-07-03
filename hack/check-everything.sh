a3d8ac719d736ab430a30cf816298ac98610ec79
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2025-07-03 14:21:26 +0200

    try with upstream/132704

--- hack/check-everything.sh
+++ hack/check-everything.sh
@@ -41,6 +41,39 @@ tmp_bin=/tmp/cr-tests-bin
 )
 export KUBEBUILDER_ASSETS="$(${tmp_bin}/setup-envtest use --use-env -p path "${ENVTEST_K8S_VERSION}")"
 
+# HACK
+k8s_clone_dir=$tmp_root/kubernetes
+(
+  k8s_repo_url=https://github.com/kubernetes/kubernetes.git
+
+  echo "Cloning Kube repository from $k8s_repo_url..."
+  git clone $k8s_repo_url $k8s_clone_dir
+
+  cd $k8s_clone_dir
+
+  pr_number="132704"
+  echo "Fetching pull request #$pr_number..."
+  git fetch origin pull/$pr_number/head:pr-$pr_number
+
+  echo "Checking out pull request #$pr_number..."
+  git checkout pr-$pr_number
+
+  echo "Building Kube from source code..."
+  make
+)
+k8s_bin_dir=$(
+  k8s_output_dir=${k8s_clone_dir}/_output/local/go/bin
+  if [ -d "${k8s_output_dir}" ]; then
+    cd ${k8s_output_dir}
+    pwd
+  else
+    echo "Directory ${k8s_output_dir} does not exist."
+    exit 1
+  fi
+)
+echo "Replacing kube-apiserver binary from ${k8s_bin_dir} to ${KUBEBUILDER_ASSETS}"
+cp -f "${k8s_bin_dir}/kube-apiserver" "${KUBEBUILDER_ASSETS}/kube-apiserver"
+
 echo "Enabling WatchListClient feature"
 export KUBE_FEATURE_WatchListClient=true
 

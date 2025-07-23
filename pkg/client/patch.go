948d554cd9c7692f21139384afaafaea8524626f
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2025-07-23 12:40:28 -0400

    :warning: Add native SSA support (#3253)
    * :sparkles: Add native SSA support
    
    This change adds native server-side apply support to the client by
    extending it with an `Apply` method that takes an
    `runtime.ApplyConfiguration`.
    
    * Field validation
    
    * Revert "Field validation"
    
    This reverts commit ad1966a958635dc7f658e6904b676088a0946701.
    
    This doesn't work, the server will always error on additional fields
    when using SSA, even when fieldValiation=None is configured.
    
    * Explain why we don't update the namespaced client
    
    * Client: Force ownership supports Apply
    
    * Fieldvalidation wrapper: Do nothing for `Apply`
    
    * Improve explanation as to why unstructured shouldn't originate from an api type
    
    * Test Apply in interceptor
    
    * Smaller fixes
    
    * Implement Apply support in the namespaced client
    
    * Apply comes first
    
    * Add tests for apply options

--- pkg/client/patch.go
+++ pkg/client/patch.go
@@ -27,6 +27,7 @@ import (
 
 var (
 	// Apply uses server-side apply to patch the given object.
+	// Deprecated: Use client.Apply instead.
 	Apply Patch = applyPatch{}
 
 	// Merge uses the raw object as a merge patch, without modifications.

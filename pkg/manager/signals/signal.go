0b18d3608f4a6a75f7308761a7afa557ffddef12
s-z-z <xiaoxin09@hotmail.com>
2025-08-28 17:08:39 +0800

    cleanup: replace custom signal handling with Kubernetes apiserver.SetupSignalContext as they are identical.
    Signed-off-by: s-z-z <xiaoxin09@hotmail.com>

--- pkg/manager/signals/signal.go
+++ pkg/manager/signals/signal.go
@@ -17,29 +17,7 @@ limitations under the License.
 package signals
 
 import (
-	"context"
-	"os"
-	"os/signal"
+	apiserver "k8s.io/apiserver/pkg/server"
 )
 
-var onlyOneSignalHandler = make(chan struct{})
-
-// SetupSignalHandler registers for SIGTERM and SIGINT. A context is returned
-// which is canceled on one of these signals. If a second signal is caught, the program
-// is terminated with exit code 1.
-func SetupSignalHandler() context.Context {
-	close(onlyOneSignalHandler) // panics when called twice
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, shutdownSignals...)
-	go func() {
-		<-c
-		cancel()
-		<-c
-		os.Exit(1) // second signal. Exit directly.
-	}()
-
-	return ctx
-}
+var SetupSignalHandler = apiserver.SetupSignalContext

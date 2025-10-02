ed5895fd315c6c7fb45c474ebec1191cedfd6a25
moritzmoe <51033452+moritzmoe@users.noreply.github.com>
2025-10-02 17:31:41 +0200

    rm async from test

--- pkg/controller/priorityqueue/priorityqueue_test.go
+++ pkg/controller/priorityqueue/priorityqueue_test.go
@@ -197,13 +197,14 @@ var _ = Describe("Controllerworkqueue", func() {
 		Expect(metrics.retries["test"]).To(Equal(1))
 	})
 
-	It("returns high priority item that became ready before low priority items", func() {
+	It("returns high priority item that became ready before low priority item", func() {
 		q, metrics := newQueue()
 		defer q.ShutDown()
 
 		now := time.Now().Round(time.Second)
 		nowLock := sync.Mutex{}
 		tick := make(chan time.Time)
+		tickSetup := make(chan any)
 
 		cwq := q.(*priorityqueue[string])
 		cwq.now = func() time.Time {
@@ -213,47 +214,28 @@ var _ = Describe("Controllerworkqueue", func() {
 		}
 		cwq.tick = func(d time.Duration) <-chan time.Time {
 			Expect(d).To(Equal(time.Second))
+			close(tickSetup)
 			return tick
 		}
-
-		retrievedItem := make(chan any)
-		getNext := make(chan any)
-
-		go func() {
-			defer GinkgoRecover()
-			defer close(retrievedItem)
-
-			key, prio, _ := q.GetWithPriority()
-			Expect(key).To(Equal("foo"))
-			Expect(prio).To(Equal(-100))
-
-			retrievedItem <- nil
-			<-getNext
-
-			key, prio, _ = q.GetWithPriority()
-			Expect(key).To(Equal("prio"))
-			Expect(prio).To(Equal(0))
-		}()
 
 		lowPriority := -100
 		highPriority := 0
 		q.AddWithOpts(AddOpts{After: 0, Priority: &lowPriority}, "foo")
-		q.AddWithOpts(AddOpts{After: 0, Priority: &lowPriority}, "bar")
 		q.AddWithOpts(AddOpts{After: time.Second, Priority: &highPriority}, "prio")
 
-		<-retrievedItem
+		Eventually(tickSetup).Should(BeClosed())
 
 		nowLock.Lock()
 		now = now.Add(time.Second)
 		nowLock.Unlock()
 		tick <- now
-		getNext <- nil
 
-		Eventually(retrievedItem).Should(BeClosed())
-		close(getNext)
+		key, prio, _ := q.GetWithPriority()
 
+		Expect(key).To(Equal("prio"))
+		Expect(prio).To(Equal(0))
 		Expect(metrics.depth["test"]).To(Equal(map[int]int{-100: 1, 0: 0}))
-		Expect(metrics.adds["test"]).To(Equal(3))
+		Expect(metrics.adds["test"]).To(Equal(2))
 		Expect(metrics.retries["test"]).To(Equal(1))
 	})
 

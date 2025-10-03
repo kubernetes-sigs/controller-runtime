c71b8c8fcf41f9661f007fd74f1766d2cfa938ef
Stefan Bueringer <buringerst@vmware.com>
2025-10-03 08:43:07 +0200

    Add Priority field to reconcile.Result
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- pkg/reconcile/reconcile.go
+++ pkg/reconcile/reconcile.go
@@ -44,6 +44,11 @@ type Result struct {
 	// RequeueAfter if greater than 0, tells the Controller to requeue the reconcile key after the Duration.
 	// Implies that Requeue is true, there is no need to set Requeue to true at the same time as RequeueAfter.
 	RequeueAfter time.Duration
+
+	// Priority is the priority that will be used if the item gets re-enqueued (also if an error is returned).
+	// If Priority is not set the original Priority of the request is preserved.
+	// Note: Priority is only respected if the controller is using a priorityqueue.PriorityQueue.
+	Priority *int
 }
 
 // IsZero returns true if this result is empty.

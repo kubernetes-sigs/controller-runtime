706924b5b807f2792ba4f877978e9bd92450f9bf
jose.vazquez <jose.vazquez@mongodb.com>
2026-01-29 16:29:17 +0100

    Also test that passing the wrong RV fails with expected error
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- pkg/client/fake/client_test.go
+++ pkg/client/fake/client_test.go
@@ -1879,7 +1879,6 @@ var _ = Describe("Fake client", func() {
 
 		resourceAC := client.ApplyConfigurationFromUnstructured(resourceForApply)
 
-		// This currently fails with a resourceVersion mismatch error, but it should succeed.
 		err := cl.Status().Apply(ctx, resourceAC, client.FieldOwner("test-owner"), client.ForceOwnership)
 		Expect(err).NotTo(HaveOccurred(), "SSA apply on status should succeed when resourceVersion is not set")
 
@@ -1896,6 +1895,44 @@ var _ = Describe("Fake client", func() {
 		}
 		Expect(cl.Get(ctx, client.ObjectKeyFromObject(finalResource), finalResource)).To(Succeed())
 		Expect(finalResource.Status.Phase).To(Equal("Ready"))
+	})
+
+	It("should block SSA apply on status when passing the wrong non empty resource version", func(ctx SpecContext) {
+		schemeBuilder := &scheme.Builder{GroupVersion: schema.GroupVersion{Group: "chaosapps.metamagical.io", Version: "v1"}}
+		schemeBuilder.Register(&ChaosPod{})
+		testScheme := runtime.NewScheme()
+		Expect(schemeBuilder.AddToScheme(testScheme)).NotTo(HaveOccurred())
+
+		customResource := &ChaosPod{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "chaosapps.metamagical.io/v1",
+				Kind:       "ChaosPod",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-chaospod",
+				Namespace: "default",
+			},
+			Spec: ChaosPodSpec{
+				Image: "test-image",
+			},
+		}
+		cl := NewClientBuilder().WithScheme(testScheme).WithStatusSubresource(customResource).WithObjects(customResource).Build()
+
+		// Create an unstructured apply configuration with status but no resourceVersion
+		resourceForApply := &unstructured.Unstructured{}
+		resourceForApply.SetName("test-chaospod")
+		resourceForApply.SetNamespace("default")
+		resourceForApply.SetAPIVersion("chaosapps.metamagical.io/v1")
+		resourceForApply.SetKind("ChaosPod")
+		resourceForApply.SetResourceVersion("bad-resource-version") // forces the error
+		Expect(unstructured.SetNestedField(resourceForApply.Object, map[string]any{"phase": "Ready"}, "status")).To(Succeed())
+
+		resourceAC := client.ApplyConfigurationFromUnstructured(resourceForApply)
+
+		// This is expected to fail with the wring rv value passed in in the applied config
+		err := cl.Status().Apply(ctx, resourceAC, client.FieldOwner("test-owner"), client.ForceOwnership)
+		Expect(err).To(HaveOccurred(), "SSA apply on status should not succeed when resourceVersion is wrongly set")
+		Expect(apierrors.IsConflict(err)).To(BeTrue())
 	})
 
 	It("should not be able to manually update the fieldManager through a status update", func(ctx SpecContext) {

c2bc87682c067ae406aea1e3f88e9827e7607595
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2026-03-08 10:41:56 +0100

    We need unstructured applyconfiguration

--- pkg/client/consistency_envtest_test.go
+++ pkg/client/consistency_envtest_test.go
@@ -166,7 +166,7 @@ var _ = Describe("ConsistentClient", func() {
 			}, nil
 		}),
 
-		FEntry("apply", func(ctx context.Context, cl client.Client, cm *corev1.ConfigMap) (writeResult, error) {
+		Entry("apply", func(ctx context.Context, cl client.Client, cm *corev1.ConfigMap) (writeResult, error) {
 			ac := corev1ac.ConfigMap(cm.Name, cm.Namespace).
 				WithData(map[string]string{"key": "applied"})
 			if err := cl.Apply(ctx, ac, client.FieldOwner("consistency-test"), client.ForceOwnership); err != nil {

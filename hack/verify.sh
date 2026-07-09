16fae8a809a393b6e834808bab4bb8909e0f8403
Stefan Büringer <4662360+sbueringer@users.noreply.github.com>
2026-07-09 15:58:32 +0200

    ✨ Add copyright header validation & fix findings (#3544)
    * Add copyright header validation
    
    Signed-off-by: Stefan Büringer buringerst@vmware.com
    
    * Fix findings
    
    ---------
    
    Signed-off-by: Stefan Büringer buringerst@vmware.com
    Co-authored-by: Stefan Bueringer <sbueringer@users.noreply.github.com>

--- hack/verify.sh
+++ hack/verify.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-#  Copyright 2018 The Kubernetes Authors.
+# Copyright 2018 The Kubernetes Authors.
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e
 
@@ -30,6 +30,8 @@ if [[ -n ${CI} ]]; then
     header_text "verifying modules"
     make verify-modules
 fi
+
+make verify-boilerplate
 
 header_text "running golangci-lint"
 make lint

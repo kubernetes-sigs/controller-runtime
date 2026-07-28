e663f792fa02386a956e129eb182a340f92f9285
Pullagura Santosha <gurutattwa@gmail.com>
2026-07-28 22:34:17 +0530

    Drop unused typed convert on scale update

--- pkg/client/fake/client.go
+++ pkg/client/fake/client.go
@@ -1649,27 +1649,15 @@ func extractScale(obj client.Object) (*autoscalingv1.Scale, error) {
 func applyScale(obj client.Object, scale *autoscalingv1.Scale) error {
 	switch obj := obj.(type) {
 	case *unstructured.Unstructured:
-		var typed client.Object
 		switch obj.GroupVersionKind() {
-		case appsv1.SchemeGroupVersion.WithKind("Deployment"):
-			typed = &appsv1.Deployment{}
-		case appsv1.SchemeGroupVersion.WithKind("ReplicaSet"):
-			typed = &appsv1.ReplicaSet{}
-		case appsv1.SchemeGroupVersion.WithKind("StatefulSet"):
-			typed = &appsv1.StatefulSet{}
-		case corev1.SchemeGroupVersion.WithKind("ReplicationController"):
-			typed = &corev1.ReplicationController{}
+		case appsv1.SchemeGroupVersion.WithKind("Deployment"),
+			appsv1.SchemeGroupVersion.WithKind("ReplicaSet"),
+			appsv1.SchemeGroupVersion.WithKind("StatefulSet"),
+			corev1.SchemeGroupVersion.WithKind("ReplicationController"):
+			return unstructured.SetNestedField(obj.Object, int64(scale.Spec.Replicas), "spec", "replicas")
 		default:
 			return fmt.Errorf("scale subresource for resource %T is not implemented", obj)
 		}
-		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, typed); err != nil {
-			return err
-		}
-		if err := applyScale(typed, scale); err != nil {
-			return err
-		}
-		// applyScale only updates Spec.Replicas for known types; write that back.
-		return unstructured.SetNestedField(obj.Object, int64(scale.Spec.Replicas), "spec", "replicas")
 	case *appsv1.Deployment:
 		obj.Spec.Replicas = new(scale.Spec.Replicas)
 	case *appsv1.ReplicaSet:
